7deaee6800719c9e4c54f549ca7402a6d634166d
backcage <111345407+backcage@users.noreply.github.com>
2023-01-09 14:14:47 +0900

    update Photon / Oculus

---
+++ GHWar/Assets/2.Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager instance;
+    
+    public bool isVR;
+
+    public static bool isPresent()
+    {
+        var list_xrDisplaySubsystem = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances<XRDisplaySubsystem>(list_xrDisplaySubsystem);
+
+        foreach(var xrDisplay in list_xrDisplaySubsystem)
+        {
+            if (xrDisplay.running)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+
+        Debug.Log("VR Device = " + isPresent().ToString());
+        isVR = isPresent();
+    }
+
+    void Start()
+    {
+        // 실행화면에 대한 해상도 960x640
+        Screen.SetResolution(960, 640, FullScreenMode.Windowed);
+
+        // 데이터 송수신 빈도 초당 30으로 설정
+        PhotonNetwork.SendRate = 30;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    void Update()
+    {
+        
+    }
+}
