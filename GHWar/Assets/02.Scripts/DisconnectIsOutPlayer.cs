557523d15ec35d56ea272ab783831b08eb139f2c
backcage <111345407+backcage@users.noreply.github.com>
2023-02-21 16:43:43 +0900

    손에 잡히게함

--- GHWar/Assets/02.Scripts/DisconnectIsOutPlayer.cs
+++ GHWar/Assets/02.Scripts/DisconnectIsOutPlayer.cs
@@ -5,8 +5,15 @@ using Photon.Pun;
 
 public class DisconnectIsOutPlayer : MonoBehaviourPun
 {
+    XRGrabInteractionPun _XRGrabInteractionPun;
+
+    private void Start()
+    {
+        _XRGrabInteractionPun = GetComponent<XRGrabInteractionPun>();    
+    }
     private void Update()
     {
+        if (_XRGrabInteractionPun.isGrab) return;
         if (photonView.IsMine)
         {
             if(photonView.ControllerActorNr != photonView.CreatorActorNr)
